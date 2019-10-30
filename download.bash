892faabe7e01c3a60ed1c3ede8ab7f51c4470665
olol85 <olol85@gmail.com>
2019-10-30 09:39:57 +0100

    load dict

--- download.bash
+++ download.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget --timestamping http://content.sniklaus.com/github/pytorch-hed/network-bsds500.pytorch
+wget --timestamping http://content.sniklaus.com/github/pytorch-hed/network-bsds500.pytorch -P $HOME/.torch/models
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='hed',
+    version='',
+    author='Laurent Lejeune',
+    author_email='laurent.lejeune@artorg.unibe.ch',
+    description='hed',
+    packages=find_packages())
